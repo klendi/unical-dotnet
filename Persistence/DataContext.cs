de9de0e69e3c95fe1fa2117080bdd58878ca15ed
Klendi Gocci <klendigocci@gmail.com>
2020-03-15 12:36:15 +0100

    Initial Commit

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Domain;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Value> Values { get; set; }
+        public DbSet<Activity> Activities { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Value>().HasData(
+                new Value {Id = 1, Name = "Value 1"},
+                new Value {Id = 2, Name = "Value 2"},
+                new Value {Id = 3, Name = "Value 3"}
+            );
+        }
+    }
+}
